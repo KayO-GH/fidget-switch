418380e7d67f2cc403a901c79a29beae5be365c0
Kwadwo Agyapon-Ntra <kwadwo.agyapon-ntra@meltwater.org>
2019-02-22 13:09:50 +0000

    Updates

---
+++ precache-manifest.a22fd64aa0b71242feba5b7b895a6d5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0eadc5e039739421b1c5047b49709f0d",
+    "url": "/fidget-switch/static/media/click.0eadc5e0.wav"
+  },
+  {
+    "revision": "3513ab6fbece7bf43f13",
+    "url": "/fidget-switch/static/js/runtime~main.3513ab6f.js"
+  },
+  {
+    "revision": "0cc30eecefd9e9cb1b03",
+    "url": "/fidget-switch/static/js/main.0cc30eec.chunk.js"
+  },
+  {
+    "revision": "aebc50e1a964c3c96299",
+    "url": "/fidget-switch/static/js/2.aebc50e1.chunk.js"
+  },
+  {
+    "revision": "0cc30eecefd9e9cb1b03",
+    "url": "/fidget-switch/static/css/main.4d3830e4.chunk.css"
+  },
+  {
+    "revision": "9859336e737d529f27c6e2f3a86f97dc",
+    "url": "/fidget-switch/index.html"
+  }
+];
