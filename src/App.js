041b7a70c92c132c851347d39ec57802132110f1
Kwadwo Agyapon-Ntra <kwadwo.agyapon-ntra@meltwater.org>
2019-02-22 14:00:56 +0000

    Added explanatory comments

--- src/App.js
+++ src/App.js
@@ -4,21 +4,26 @@ import offSwitch from './images/off.png';
 import audio from './audio/click.wav';
 
 const App = () => {
+  //set up local state
   const [isOn, setIsOn] = useState(true);
 
+  //change background on state change
   useEffect(() => {
     document.body.style.backgroundImage = isOn ?
       "radial-gradient(circle at top,#F1EFEF, #B0B0B3)" :
       "radial-gradient(circle at top,#5A5A5A, #2C2B2B)";
   });
 
+  //set up audio player
   const player = new Audio(audio);
 
+  //click handler
   const handleClick = () => {
     setIsOn(!isOn);
     player.play();
   }
 
+  //render component
   return (
     <div className="App App-header">
       <img src={isOn ? onSwitch : offSwitch} alt="switch" onClick={handleClick} />
